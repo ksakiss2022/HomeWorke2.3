3734441242778223bfb9d04eb923c720a5262f6b
Swetlana <sweta_zaika@mail.ru>
2022-10-10 21:11:19 +0300

    дз 2.3. попытка 2

--- src/Flower.java
+++ src/Flower.java
@@ -1,3 +1,105 @@
 public class Flower {
+    public String getFlowerColorName() {
+        return flowerColorName;
+    }
 
+    public void setFlowerColorName(String flowerColorName) {
+        if (flowerColorName != null && !flowerColorName.isBlank() && !flowerColorName.isEmpty()) {
+            this.flowerColorName = flowerColorName;
+        } else {
+            this.flowerColorName = "белый";
+        }
+    }
+
+    //Цвет цветка (flowerColor);
+    private String flowerColorName;
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        if (country != null && !country.isEmpty() && !country.isBlank()) {
+
+            this.country = country;
+        } else {
+            this.country = "Россия";
+        }
+    }
+
+    //Страна происхождения (country);
+    private String country;
+
+    public float getCost() {
+        return cost;
+    }
+
+    public void setCost(float cost) {
+        if (cost > 0) {
+            this.cost = cost;
+        } else {
+            this.cost = 1.00f;
+        }
+    }
+
+    //Стоимость (cost);
+    private float cost;
+
+    public int getLifeSpan() {
+        return lifeSpan;
+    }
+
+    public void setLifeSpan(int lifeSpan) {
+        if (lifeSpan <= 0) {
+            this.lifeSpan = 3;
+        } else {
+            this.lifeSpan = Math.abs(lifeSpan);
+        }
+
+    }
+
+    //Срок стояния, указанный в целых днях (lifeSpan).
+    private int lifeSpan;
+
+    Flower(String flowerColorName, String country, float cost, int lifeSpan) {
+
+        this.flowerColorName = flowerColorName;
+
+        if (flowerColorName != null && !flowerColorName.isBlank() && !flowerColorName.isEmpty()) {
+            this.flowerColorName = flowerColorName;
+        } else {
+            this.flowerColorName = "белый";
+        }
+
+
+        this.country = country;
+        if (country != null && !country.isEmpty() && !country.isBlank()) {
+
+            this.country = country;
+        } else {
+            this.country = "Россия";
+        }
+
+        this.cost = cost;
+        if (cost > 0) {
+            this.cost = cost;
+        } else {
+            this.cost = 1.00f;
+        }
+
+        this.lifeSpan = lifeSpan;
+        if (lifeSpan <= 0) {
+            this.lifeSpan = 3;
+        } else {
+            this.lifeSpan = Math.abs(lifeSpan);
+        }
+
+    }
+
+    void printflower() {
+        System.out.println("Название- " + flowerColorName + "," + " страной происхождения- " + country + "," + " стоимость- "
+                + cost + ", " + "срок стояния цветка- " + lifeSpan);
+
+
+    }
 }
--- src/Human.java
+++ src/Human.java
@@ -1,5 +1,64 @@
 public class Human {
 
 
+    public int getYearOfBirth() {
+        return yearOfBirth;
+    }
 
+    public void setYearOfBirth(int yearOfBirth) {
+        if (yearOfBirth >= 0) {
+            this.yearOfBirth = yearOfBirth;
+        } else {
+            this.yearOfBirth = 0;
+        }
+    }
+
+    private int yearOfBirth;
+    String name;
+
+    public String getTown() {
+        return town;
+    }
+
+    public void setTown(String town) {
+        if (town == null && !town.isEmpty() && !town.isBlank()) {
+            this.town = town;
+        } else {
+            this.town = "Информация не указана";
+        }
+    }
+
+    String town;
+    String job;
+
+    Human(int yearOfBirth, String name, String town, String job) {
+
+        this.name = name;
+        if (name == null) {
+            this.name = "Информация не указана";
+        }
+        this.town = town;
+        if (town == null) {
+            this.town = "Информация не указана";
+        }
+        this.job = job;
+        if (job == null) {
+            this.job = "Информация не указана";
+        }
+        if (yearOfBirth >= 0) {
+            this.yearOfBirth = yearOfBirth;
+        } else {
+            this.yearOfBirth = 0;
+        }
+    }
+
+
+    void human() {
+
+        System.out.print("Привет! Меня зовут " + name + ". ");
+        System.out.print("Я из города " + town + ". ");
+        System.out.print("Мне " + yearOfBirth + " лет. ");
+        System.out.println("Я работаю на должности" + job + ". Будем знакомы!");
+        System.out.println();
+    }
 }
